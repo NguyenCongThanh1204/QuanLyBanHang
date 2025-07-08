053e3a46bf59e9182134b95d8c37edf3b6f17ce5
QuocTan <nguyenquoctan3@dtu.edu.vn>
2025-07-08 16:52:13 +0700

    QUanlysanpham

---
+++ QuanLyBanHang/Pages/QuanLySanPham.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using QuanLyBanHang.Models;
+
+namespace QuanLyBanHang.Pages
+{
+    public class QuanLySanPhamModel : PageModel
+    {
+        private readonly QLBanGiayContext _context;
+
+        public QuanLySanPhamModel(QLBanGiayContext context)
+        {
+            _context = context;
+        }
+
+        public List<SanPham> DanhSachSanPham { get; set; } = new();
+
+        public void OnGet()
+        {
+            DanhSachSanPham = _context.SanPhams
+                .AsNoTracking() // tối ưu hiệu năng đọc
+                .ToList();
+        }
+    }
+}
